f52c8da70625f1c2d28981205bcf1544dd129039
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-10 14:39:04 +0200

    Add error checking on negatives or nan (sumAll())

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function (from, to) {
   let output = 0;
-  if (from < 0 || to < 0) {
+  if (
+    from < 0 ||
+    to < 0 ||
+    typeof from !== 'number' ||
+    typeof to !== 'number'
+  ) {
     return 'ERROR';
   }
   if (from > to) {
