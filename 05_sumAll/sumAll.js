ddf684f626f90d4fbc62913611c2529fa2507930
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-10 14:18:18 +0200

    Add return error on negative num (sumaAll) JEST

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,14 @@
 const sumAll = function (from, to) {
   let output = 0;
+  if (from < 0 || to < 0) {
+    return 'ERROR';
+  }
   if (from > to) {
     [from, to] = [to, from];
   }
   for (let i = from; i <= to; i++) {
     output += i;
   }
-  const numbers = [10, 20, 30, 40];
-
-  // Skips the second element
-  const [first, , third] = numbers;
-  console.log(first);
-  console.log(third);
   return output;
 };
 //code finished
