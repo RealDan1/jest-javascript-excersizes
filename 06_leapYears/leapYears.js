80bea60fee7b484092e270862bb06ef465d2f350
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-12 14:31:46 +0200

    Add 4 tests passed, 5th test WIP, Leapyears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,7 +2,7 @@ const leapYears = function (inputYear) {
   let isLeapYear = false;
   if (
     (inputYear % 4 === 0 || inputYear % 400 === 0) &&
-    !inputYear % 100 === 0
+    !(inputYear % 100 === 0)
   ) {
     isLeapYear = true;
   }
