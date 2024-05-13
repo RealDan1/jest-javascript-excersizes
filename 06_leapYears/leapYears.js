b6425f89f69a4dcb6ddba0596c2c3dbc55956b4c
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-13 13:07:16 +0200

    Add comment for: Bug in conflicting if statement

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,6 +3,7 @@ const leapYears = function (inputYear) {
   if (
     (inputYear % 4 === 0 || inputYear % 400 === 0) &&
     !(inputYear % 100 === 0)
+    // divisible by 100 and 400 is conflicting? 1600 returns false
   ) {
     isLeapYear = true;
   }
@@ -12,5 +13,6 @@ const leapYears = function (inputYear) {
 //Leap years are years divisible by four (like 1984 and 2004). However,
 //years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400
 //(like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+
 // Do not edit below this line
 module.exports = leapYears;
