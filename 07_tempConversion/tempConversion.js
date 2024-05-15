9809a03768799c2368014cee35d43aea8eb3104d
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-15 14:06:54 +0200

    Add tempConversion - Pass all JEST Tests (Assigment complete)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farhenheitInput) {
+  let celsiusOutput = (farhenheitInput - 32) * (5 / 9);
+  celsiusOutput = celsiusOutput.toFixed(1);
+  return Number(celsiusOutput);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusInput) {
+  let farhenheitOutput = celsiusInput * (9 / 5) + 32;
+  farhenheitOutput = farhenheitOutput.toFixed(1);
+  return Number(farhenheitOutput);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
