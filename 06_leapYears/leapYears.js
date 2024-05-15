7a2dea39d08b8158e1444b56b07db11f93182a94
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-15 10:22:12 +0200

    Fix IF ELSE conflict for leapyears.js and pass all JEST tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,14 @@
 const leapYears = function (inputYear) {
   let isLeapYear = false;
-  if (
-    (inputYear % 4 === 0 || inputYear % 400 === 0) &&
-    !(inputYear % 100 === 0)
-    // divisible by 100 and 400 is conflicting? 1600 returns false
-    //try inputting just the two or statements on the first if, then !(100) on the
-    //second and another bolean to confirm for the conflicting scenario. Should work
-  ) {
-    isLeapYear = true;
-    //
+
+  if (inputYear % 4 === 0) {
+    if (inputYear % 400 === 0) {
+      isLeapYear = true;
+    } else if (inputYear % 100 === 0) {
+      inputYear = false;
+    } else {
+      isLeapYear = true;
+    }
   }
 
   return isLeapYear;
@@ -16,6 +16,10 @@ const leapYears = function (inputYear) {
 //Leap years are years divisible by four (like 1984 and 2004). However,
 //years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400
 //(like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+
+//Interesting IF ELSE logic problem encountered:
+//divisible by 100 and 400 is conflicting? 1600 returns false
+//try inputting just the two or statements on the first if, then !(100) on the second and another bolean to confirm for the conflicting scenario. Should work
 
 // Do not edit below this line
 module.exports = leapYears;
